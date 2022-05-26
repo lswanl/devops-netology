a9bbf12017e95261de5ab1f73116cf463fd2c7f0
Denis <lswanl@ya.ru>
2022-05-26 12:20:20 +0300

    04-script-02-py

---
+++ 3script_py.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import os
+
+bash_command = ["cd ~/dlebedev/GIT/devops-netology/", "git status"]
+result_os = os.popen(' && '.join(bash_command)).read()
+#is_change = False
+for result in result_os.split('\n'):
+    if result.find('modified') != -1:
+        prepare_result = result.replace('\tmodified:   ', '')
+        print(os.path.abspath(prepare_result))
+#        break
